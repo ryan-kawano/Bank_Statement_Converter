75f0e19e24e6cf7f9d7850955a263c3042abbf24
rkawano518 <rkawano518@gmail.com>
2025-07-10 21:38:15 -0700

    Update to use smart pointers instead of raw pointers

--- include/bank_statement_converter/quick_sort.h
+++ include/bank_statement_converter/quick_sort.h
@@ -7,6 +7,8 @@
 
 #include <random>
 
+#include <rk_logger/logger.h>
+
 /**
  * @class QuickSort
  * @brief Implements the Quick Sort algorithm.
@@ -20,13 +22,13 @@ public:
      * Chooses a random pivot by using the std::mt19937 random-number generator. Moves everything less than or equal
      * to the pivot, to the left of the pivot by using std::swap.
      * 
-     * @param std::vector<T*> The vector to sort.
+     * @param std::vector<std::shared_ptr<T>> The vector to sort.
      * @param int The lowest index of the portion to sort.
      * @param int The highest index of the portion to sort.
      * 
      * @return The pivot index.
      */
-    static int partition(std::vector<T*>& vec, int lower, int higher) {
+    static int partition(std::vector<std::shared_ptr<T>>& vec, int lower, int higher) {
         RK_LOG("Entering partition func\n");
 
         std::random_device rd; // For generating random pivot. Optimize this later
@@ -36,7 +38,7 @@ public:
         int randomPivotIdx = dist(gen);
 
         std::swap(vec[randomPivotIdx], vec[higher]); // Move pivot to the end
-        T* pivot = vec[higher];
+        std::shared_ptr<T> pivot = vec[higher];
         int i = lower - 1; // For tracking lower numbers
 
         // Go through vector and move numbers less than or equal to the pivot, to the left of pivot
@@ -57,11 +59,11 @@ public:
     /**
      * @brief Recursively executes the Quick Sort algorithm.
      * 
-     * @param std::vector<T*> The vector to sort.
+     * @param std::vector<std::shared_ptr<T>> The vector to sort.
      * @param int The lowest index of the portion to sort.
      * @param int The highest index of the portion to sort.
      */
-    static void quickSort(std::vector<T*>& vec, int lower, int higher) {
+    static void quickSort(std::vector<std::shared_ptr<T>>& vec, int lower, int higher) {
         RK_LOG("Entering quickSort func\n");
 
         if (lower < higher) {
