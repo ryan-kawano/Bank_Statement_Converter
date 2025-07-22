25601ec091c59f949b33fdf976c642e7bcd13e08
rkawano518 <rkawano518@gmail.com>
2025-07-22 01:52:54 -0700

    Fix formatting

--- src/constants.cpp
+++ src/constants.cpp
@@ -1,7 +1,11 @@
-#include "bank_statement_converter/constants.h"
+#include <bank_statement_converter/constants.h>
 
+namespace bsc {
 namespace constants {
-    const std::unordered_map<std::string, constants::Bank> BANKS = {
-        { "Wells Fargo", constants::Bank::WELLS_FARGO }
+
+    const std::unordered_map<std::string, bsc::constants::Bank> BANKS = {
+        { "Wells Fargo", bsc::constants::Bank::WELLS_FARGO }
     };
+
 } // namespace constants
+} // namespace bsc
