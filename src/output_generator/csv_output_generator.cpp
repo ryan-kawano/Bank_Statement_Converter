1225dc0b2677ac04de308a2ecea538364dcc2525
rkawano518 <rkawano518@gmail.com>
2025-07-22 02:04:00 -0700

    Finish logic for generating CSV files

--- src/output_generator/csv_output_generator.cpp
+++ src/output_generator/csv_output_generator.cpp
@@ -1,15 +1,23 @@
 #include <bank_statement_converter/output_generator/csv_output_generator.h>
+#include <bank_statement_converter/transaction/transaction_adapter.h>
+#include <rk_logger/logger.h>
 
-void CsvOutputGenerator::generate(const std::vector<std::shared_ptr<IStatement>>& data, const std::filesystem::path& outputPath) {
+namespace bsc {
+
+void CsvOutputGenerator::generate(const std::vector<std::shared_ptr<IStatement>>& statements, const std::filesystem::path& outputPath) {
     RK_LOG("Generating CSV output\n");
 
     std::ofstream outFile(outputPath);
-
     if (!outFile) {
         RK_LOG("Unable to open output file for CSV generation\n");
         return;
     }
-    /*
-    TO DO
-    */
+
+    for (const auto& statement : statements) {
+        for (const auto& transaction : statement->getTransactions()) {
+            outFile << bsc::TransactionAdapter::toCsv(transaction) << "\n";
+        }
+    }
 }
+
+} // namespace bsc
