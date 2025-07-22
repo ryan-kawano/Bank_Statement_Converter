c9fe732bd4a14f5b6f216c2fdeec09ea9a0e1f79
rkawano518 <rkawano518@gmail.com>
2025-07-22 01:51:48 -0700

    Update factory methods to be static as instances aren't needed

--- include/bank_statement_converter/output_generator/output_generator_factory.h
+++ include/bank_statement_converter/output_generator/output_generator_factory.h
@@ -3,12 +3,24 @@
 
 #include <memory>
 
+#include <bank_statement_converter/output_generator/csv_output_generator.h>
 #include <bank_statement_converter/interfaces/i_statement_output_generator.h>
 #include <bank_statement_converter/output_generator/output_types.h>
 
+namespace bsc {
+
 class OutputGeneratorFactory {
 public:
-    std::unique_ptr<IStatementOutputGenerator> createOutputGenerator(const Output::Format format) const;
+    static std::unique_ptr<IStatementOutputGenerator> createOutputGenerator(const bsc::output::Format format) {
+        switch (format) {
+            case bsc::output::Format::CSV:
+                return std::make_unique<CsvOutputGenerator>();
+            default:
+                throw std::invalid_argument("Unsupported FileType");
+        }
+    }
 };
+
+} // namespace bsc
 
 #endif // #ifndef OUTPUT_GENERATOR_FACTORY_H
--- include/bank_statement_converter/statement_processor/statement_processor_factory.h
+++ include/bank_statement_converter/statement_processor/statement_processor_factory.h
@@ -1,13 +1,24 @@
 #ifndef STATEMENT_PROCESSOR_FACTORY_H
 #define STATEMENT_PROCESSOR_FACTORY_H
-#include <memory>
 
+#include <bank_statement_converter/constants.h>
 #include <bank_statement_converter/interfaces/i_statement_processor.h>
-#include <bank_statement_converter/statement/statement_file_format.h>
+#include <bank_statement_converter/statement_processor/wells_fargo_statement_processor.h>
+
+namespace bsc {
 
 class StatementProcessorFactory {
 public:
-    std::unique_ptr<IStatementProcessor> createProcessor(StatementFileFormat::Format) const;
+    static std::unique_ptr<IStatementProcessor> createStatementProcessor(const bsc::constants::Bank bank) {
+        switch (bank) {
+            case bsc::constants::Bank::WELLS_FARGO:
+                return std::make_unique<WellsFargoStatementProcessor>();
+            default:
+                throw std::invalid_argument("Unsupported bank type");
+        }
+    }
 };
+
+} // namespace bsc
 
 #endif // #ifndef STATEMENT_PROCESSOR_FACTORY_H
