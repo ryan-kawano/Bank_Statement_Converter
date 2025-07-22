e7a778be6a53c91a3fc42c54b9ae617c58e009d1
rkawano518 <rkawano518@gmail.com>
2025-07-22 02:03:30 -0700

    Minor updates to readability

--- src/configuration/configuration.cpp
+++ src/configuration/configuration.cpp
@@ -1,9 +1,18 @@
 #include <fstream>
 
 #include <rk_logger/logger.h>
+#include <bank_statement_converter/constants.h>
 #include <bank_statement_converter/configuration/configuration.h>
 
+namespace bsc {
+
 void Configuration::readConfigFile(const std::filesystem::path& path) {
+    /* Verify that the config file is valid */
+    if (path.empty()) {
+        RK_LOG("Path was empty\n");
+        return;
+    }
+
     RK_LOG("Reading config file from path: ", path.string(), "\n");
 
     if (!std::filesystem::exists(path)) {
@@ -18,48 +27,55 @@ void Configuration::readConfigFile(const std::filesystem::path& path) {
     }
 
     YAML::Node configNode = config["config"];
-    if (configNode && configNode.IsMap()) {
-        if (configNode["statements_directory"].IsDefined()) {
-            statementsDirectory = configNode["statements_directory"].as<std::string>();
-        } else {
-            RK_LOG("statements_directory not found in config, using default");
-        }
-        
-        if (configNode["statement_file_formats"].IsDefined()) {
-            statementFileFormats.clear();
-            for (const auto& format : configNode["statement_file_formats"]) {
-                statementFileFormats.insert(format.as<std::string>());
-            }
-        } else {
-            RK_LOG("statement_file_formats not found in config, using default");
-        }
-        
-        if (configNode["output_formats"].IsDefined()) {
-            outputFormats.clear();
-            for (const auto& format : configNode["output_formats"]) {
-                outputFormats.insert(format.as<std::string>());
-            }
-        } else {
-            RK_LOG("output_formats not found in config, using default");
-        }
-        
-        if (configNode["banks"].IsDefined()) {
-            banks.clear();
-            for (const auto& bank : configNode["banks"]) {
-                banks.insert(bank.as<std::string>());
-            }
-        } else {
-            RK_LOG("banks not found in config, using default");
-        }
-        
-        if (configNode["output_one_file"].IsDefined()) {
-            outputOneFile = configNode["output_one_file"].as<bool>();
-        } else {
-            RK_LOG("output_one_file not found in config, using default");
+    if (!configNode || !configNode.IsMap()) {
+        RK_LOG("Error reading config node\n");
+        return;
+    }
+
+    /* Read and save values from the config */
+    if (configNode["statements_directory"].IsDefined()) {
+        statementsDirectory = configNode["statements_directory"].as<std::string>();
+    } else {
+        RK_LOG("statements_directory not found in config, using default");
+    }
+    
+    if (configNode["statement_file_formats"].IsDefined()) {
+        statementFileFormats.clear();
+        for (const auto& format : configNode["statement_file_formats"]) {
+            statementFileFormats.insert(format.as<std::string>());
         }
     } else {
-        RK_LOG("Config section not found or invalid, using default settings");
+        RK_LOG("statement_file_formats not found in config, using default");
+    }
+    
+    if (configNode["output_formats"].IsDefined()) {
+        outputFormats.clear();
+        for (const auto& format : configNode["output_formats"]) {
+            outputFormats.insert(format.as<std::string>());
+        }
+    } else {
+        RK_LOG("output_formats not found in config, using default");
+    }
+    
+    if (configNode["banks"].IsDefined()) {
+        banks.clear();
+        for (const auto& bank : configNode["banks"]) {
+            if (bsc::constants::BANKS.count(bank.as<std::string>()) == 0) {
+                RK_LOG("Unsupported bank found in config: ", bank.as<std::string>(), "\n");
+            }
+            banks.insert(bank.as<std::string>());
+        }
+    } else {
+        RK_LOG("banks not found in config, using default");
+    }
+    
+    if (configNode["output_one_file"].IsDefined()) {
+        outputOneFile = configNode["output_one_file"].as<bool>();
+    } else {
+        RK_LOG("output_one_file not found in config, using default");
     }
 
     RK_LOG("Config file read successfully\n");
 }
+
+} // namespace bsc
