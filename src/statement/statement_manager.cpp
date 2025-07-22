c9d764a94a6aaa305ca6962955ec4668be9b0a09
rkawano518 <rkawano518@gmail.com>
2025-07-22 02:04:40 -0700

    Add checks to prevent saving null statements. Update sort() to sort the statements themselves as well. Update getCount() to match return value in header.

--- src/statement/statement_manager.cpp
+++ src/statement/statement_manager.cpp
@@ -2,27 +2,40 @@
 #include <bank_statement_converter/statement/statement_manager.h>
 #include <bank_statement_converter/constants.h>
 #include <bank_statement_converter/statement_processor/pdf_statement_processor.h>
-#include <bank_statement_converter/statement_processor/statement_processor_factory.h>
+#include <bank_statement_converter/statement_processor/statement_file_processor_factory.h>
+#include <bank_statement_converter/quick_sort.h>
+
+namespace bsc {
 
 void StatementManager::parseStatements(const std::vector<StatementFileManager::File>& files) {
     RK_LOG("Processing ", files.size(), " statements\n");
 
-    StatementProcessorFactory factory;
+    StatementFileProcessorFactory factory;
 
     // Parse the list of pdf files, extract the transaction data, and save it in a list
     for (const auto& file : files) {
         RK_LOG("Processing file: ", file.first, "\n");
 
-        std::unique_ptr<IStatementProcessor> statementProcessor = factory.createProcessor(file.second);
-        std::shared_ptr<IStatement> statement = statementProcessor->processFile(file.first);
-        statements.push_back(statement);
+        std::shared_ptr<IStatement> statement = StatementFileProcessorFactory::createProcessor(file.second)->processFile(file.first);
+        if (statement) {
+            statements.push_back(statement);
+        }
+        else {
+            skippedFiles.push_back(file.first);
+        }
     }
 }
 
 void StatementManager::sort() {
+    RK_LOG("Sorting ", statements.size(), " statements\n");
+
+    // Sort the transactions within the statements first
     for (const auto& statement : statements) {
         statement->sort();
     }
+
+    // Then, sort the statements themselves
+    QuickSort<IStatement>::quickSort(statements, 0, statements.size() - 1);
 }
 
 const StatementManager::Statements& StatementManager::getStatements() const {
@@ -39,6 +52,8 @@ const IStatement::SkippedLines StatementManager::getSkippedLines() const {
     return result;
 }
 
-const int StatementManager::getCount() const {
+size_t StatementManager::getCount() const {
     return statements.size();
 }
+
+} // namespace bsc
