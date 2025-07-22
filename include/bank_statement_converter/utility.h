c8c63d72b3825f55562bcba5e797485c41a6a766
rkawano518 <rkawano518@gmail.com>
2025-07-22 01:53:57 -0700

    Add a Utility module

---
+++ include/bank_statement_converter/utility.h
@@ -0,0 +1,20 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <string>
+
+namespace bsc {
+namespace utility {
+
+    /**
+     * @brief Trims leading and trailing whitespaces.
+     * 
+     * @param std::string The string to trim.
+     * @return std::string The trimmed string.
+     */
+    std::string trim(const std::string);
+
+}
+}
+
+#endif // #ifndef UTILITY_H
