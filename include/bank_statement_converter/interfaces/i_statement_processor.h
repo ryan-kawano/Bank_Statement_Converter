4d301ffc8f68556849f3d61a3c9b12b27b6b7642
rkawano518 <rkawano518@gmail.com>
2025-07-22 01:59:04 -0700

    Add "virtual" to destructor. Remove unused function.

--- include/bank_statement_converter/interfaces/i_statement_processor.h
+++ include/bank_statement_converter/interfaces/i_statement_processor.h
@@ -1,18 +1,16 @@
 #ifndef I_STATEMENT_PROCESSOR_H
 #define I_STATEMENT_PROCESSOR_H
 
-#include <filesystem>
-#include <memory>
+#include <bank_statement_converter/interfaces/i_statement_file_processor.h>
 
-#include "bank_statement_converter/interfaces/i_statement.h"
+namespace bsc {
 
 class IStatementProcessor {
 public:
-    ~IStatementProcessor() = default;
-    virtual std::shared_ptr<IStatement> processFile(const std::filesystem::path&) = 0;
-
-private:
-    virtual void openFile(const std::filesystem::path&) const = 0;
+    virtual ~IStatementProcessor() = default;
+    virtual void processLines(std::shared_ptr<IStatement>, bsc::IStatementFileProcessor::StatementLines) const = 0;
 };
+
+} // namespace bsc
 
 #endif // #ifndef I_STATEMENT_PROCESSOR_H
